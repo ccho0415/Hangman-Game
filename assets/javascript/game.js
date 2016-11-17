22ffe1ff10a82c66fdbd7ce3abd53b5819bef517
ccho0415 <ccho0415@gmail.com>
2016-11-16 19:04:48 -0500

    added key recognition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Creating the Array for all the Champions in the game as of 11-15-16
-var championarr = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu" ,"Anivia" ,"Annie" ,"Ashe" , "Aurelion Sol" , "Azir" ,
+var champions = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu" ,"Anivia" ,"Annie" ,"Ashe" , "Aurelion Sol" , "Azir" ,
     "Bard" , "Blitzcrank" , "Brand", "Braum" , "Caitlyn" , "Cassiopeia" , "Chogath" ,"Corki" , "Darius", "Diana", "Dr Mundo" ,
     "Draven", "Ekko" , "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank" ,"Garen", "Gnar", 
     "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", 
@@ -10,65 +10,66 @@ var championarr = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu" ,"Anivia" ,"Ann
     "Skarner", "Sona", "Soraka" , "Swain", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana" , "Trundle", 
     "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Vegiar", "VelKoz", "Vi", "Viktor", "Vladimir" , "Volibear" , 
     "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac" , "Zed", "Ziggs", "Zilean", "Zyra"];
-//Creating an object for messages
-messages = {
-    win: "W",
-    lose: "L"
-};
+
 // Creating variable for game related stuff
-var display =  document.querySelector("#dummy");
-var letters = "abcdefghijklmnopqrstuvwxyz";
+var randomWord1 = Math.floor(Math.random()*champions.length);
+var word = champions[randomWord1];
+var textTyped = ""; 
 var wins = 0;
 var losses = 0;
 var chances = 5;
-var randomWord1 = championarr[Math.floor(Math.random()*championarr.length)];
-var dummydiv = document.getElementById("dummy");
+document.querySelector("#dummy").innerHTML = word;
+document.querySelector("#wins").innerHTML = wins;
 
 
-//seperate the word into seperate objects
-var championstr = randomWord1;
-var championres = championstr.split("");
+// //seperate the word into seperate objects
+// var championstr = randomWord1;
+// var championres = championstr.split("");
 
-//Console Print area here so I can see whats going on
-// console.log(championarr[3]) -> Works so it seems like the championarr works
-// console.log(randomWord1);
-// console.log(weoweo);
-console.log(championres);
+// //Console Print area here so I can see whats going on
+// // console.log(championarr[3]) -> Works so it seems like the championarr works
+// // console.log(randomWord1);
+// // console.log(weoweo);
+// console.log(championres);
 
 //recording keystrokes written
 document.onkeyup = function(event){
-    displayrandomword1()
-    blanks()
     var key = event.key;
 
 
-    if ((key===championres[""])){
+    if ((textTyped == word)){
         console.log("you pressed the right key!")
+        wins ++;
+        document.querySelector("#wins").innerHTML = wins;
+        ranNum = Math.floor(Math.random()*champions.length);
         // Add append option
+    } else{
+        textTyped = textTyped + key;
+        document.querySelector("#output").innerHTML = textTyped;
     }
 
 
 
 }
-//referencing document and rewriting it
-function blanks(){
-    for (var i = 0; i < randomWord1.length; i++) {
+// //referencing document and rewriting it
+// function blanks(){
+//     for (var i = 0; i < randomWord1.length; i++) {
 
-      var newblankspace = document.createElement("span");
+//       var newblankspace = document.createElement("span");
 
-      newblankspace.innerHTML = "_ ";
+//       newblankspace.innerHTML = "_ ";
 
-      dummydiv.append(newblankspace);
+//       dummydiv.append(newblankspace);
 
-      console.log(randomWord1);
-  }
+//       console.log(randomWord1);
+//   }
 
-    }
-//display the word
-function displayrandomword1(string){
-    var string = "<p>" + randomWord1 + "</p>";
-    display.innerHTML = string;
-}
+//     }
+// //display the word
+// function displayrandomword1(string){
+//     var string = "<p>" + randomWord1 + "</p>";
+//     display.innerHTML = string;
+// }
 
 
 
